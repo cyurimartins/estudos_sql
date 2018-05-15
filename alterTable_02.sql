983c8e8b0db3115f458a213c826ae580fa8433c1
Yuri Martins <cyuri.martins@gmail.com>
2018-05-15 00:56:26 -0300

    Update alterTable_02.sql

--- alterTable_02.sql
+++ alterTable_02.sql
@@ -1,3 +1,7 @@
 alter table cliente
   change nome 
-  name varchar(80);
+  name varchar(80), --ALTERANDO O NOME DA COLUNA NOME
+  add column email varchar(20);   -- ADICIONANDO A COLUNA EMAIL 
+  drop telefone; --apagando a coluna telefone da tabela
+
+
