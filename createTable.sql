f81887bb953d8663cd56df98360321b663b3bf3f
Yuri Martins <cyuri.martins@gmail.com>
2018-05-15 00:21:26 -0300

    Update createTable.sql

--- createTable.sql
+++ createTable.sql
@@ -1,7 +1,7 @@
 -- CRIANDO UMA TABELA PESSOA SE NÃO EXISTIR NO BANCO
 
 create table if not exists Pessoa (
-	codigo integer not null, -- não permite o valor nulo nesse campo
+	codigo integer not null auto_increment, -- não permite o valor nulo nesse campo e é um campo auto incremento
     	nome varchar(60) not null default "SEM NOME", -- Atribuindo um valor default ao campo nome
     	cpf varchar(14),
     	data_nascimento date,
